7c192106407cd3c0ae00593e0d51c778e4524f5e
Dominik Oswald <6849456+d-oit@users.noreply.github.com>
2026-06-20 14:30:35 +0000

    fix: disable body-max-line-length in commitlint for bot-generated PRs

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,5 +1,8 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [0],
+  },
   ignores: [
     (message) => message.includes('Co-authored-by: codacy-production[bot]'),
     (message) => /^Update .*/.test(message) && message.includes('bot'),
